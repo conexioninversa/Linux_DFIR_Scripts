51bc05cbc756fe976fb4c896cf8d3c7d64acee94
PEDRO SANCHEZ CORDERO <pedro.sanchez@conexioninversa.com>
2021-02-05 18:29:20 +0100

    Update nix-DirectoryCreation.sh

--- nix-DirectoryCreation.sh
+++ nix-DirectoryCreation.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Last updated 05 September 2019 by Brian Moran (brian@brimorlabs.com)
-# Please read "ReadMe.txt" for more information regarding GPL, the script itself, and changes
-# RELEASE DATE: 20190905
-# AUTHOR: Brian Moran (brian@brimorlabs.com)
-# TWITTER: BriMor Labs (@BriMorLabs)
-# Version: Live Response Collection (Cedarpelta Build - 20190905)
-# Copyright: 2013-2019, Brian Moran
-
-# This file is part of the Live Response Collection
-# The Live Response Collection is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# Additionally, usages of all tools fall under the express license agreement stated by the tool itself.
-
 modulesource=${BASH_SOURCE[0]}
 modulename=${modulesource/$modulepath/}
 
